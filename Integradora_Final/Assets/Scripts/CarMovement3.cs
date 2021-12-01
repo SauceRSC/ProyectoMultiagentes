af1b414fca71c0dcee539d0d6321adc01edcf198
Ivann Llamas <ivllamoficial@gmail.com>
2021-12-01 16:26:57 -0600

    Project with simple backend integrated and unity

---
+++ Integradora_Final/Assets/Scripts/CarMovement3.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement3 : MonoBehaviour
+{
+    private bool semafo;
+    private Vector3 movimiento = Vector3.right;
+
+    [SerializeField] private float speed = 8.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Death"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        semafo = GameObject.Find("Controlador").GetComponent<Requesting>().semaforo;
+
+        transform.position += speed * Time.deltaTime * movimiento;
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position,movimiento,out hit, 5f))
+        {
+            if (hit.transform.tag == "Stop" && semafo == true)
+            {
+                speed = 0;
+            }
+            else if (hit.transform.tag == "Carrito")
+            {
+                
+                speed = 0;
+            }
+            else
+            {
+                
+                speed = 10;
+            }
+            
+
+        }
+        else
+        {
+            speed = 10;
+        }
+
+    }
+}
